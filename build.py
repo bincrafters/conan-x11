41f191671e93dc91afe7ae3c481869eb2f2bac0f
Uilian Ries <uilianries@gmail.com>
2019-08-15 14:16:51 -0300

    User lowe case for scripts names
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,6 +4,7 @@
 import json
 import subprocess
 import sys
+import tempfile
 from bincrafters import build_template_default, build_shared
 from conans import tools
 
@@ -14,13 +15,16 @@ if __name__ == "__main__":
     with open("groups.json") as json_file:
         json_data = json.load(json_file)
 
+    cache_folder = tempfile.mkdtemp("conan")
+    docker_args = "'-v ${PWD}:/home/conan/.conan/data'"
     for packages in json_data.values():
         for package in packages:
-            recipe = "conanfile-{}.py".format(package)
+            recipe = "conanfile-{}.py".format(package.lower())
             version = build_shared.get_version_from_recipe(recipe)
             with tools.environment_append({
                 "CONAN_CONANFILE": recipe,
-                "CONAN_VERSION": version
+                "CONAN_VERSION": version,
+                "CONAN_DOCKER_RUN_OPTIONS": docker_args
                 }):
-                builder = build_template_default.get_builder()
+                builder = build_template_default.get_builder(docker_args)
                 builder.run()
