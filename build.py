2d176840fb4a3b8fc546737475695ff106412525
Uilian Ries <uilianries@gmail.com>
2019-08-15 12:47:49 -0300

    Add minimum support for CI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import json
+import subprocess
+import sys
 from bincrafters import build_template_default
+from conans import tools
 
 if __name__ == "__main__":
+    subprocess.check_call([sys.executable, "manage.py", "gen"])
+    subprocess.check_call([sys.executable, "manage.py", "groups"])
 
-    builder = build_template_default.get_builder()
+    with open("groups.json") as json_file:
+        json_data = json.load(json_file)
 
-    builder.run()
+    for packages in json_data.values():
+        print(packages)
+        for package in packages:
+            with tools.environment_append({"CONAN_CONANFILE": "conanfile-{}.py".format(package)}):
+                builder = build_template_default.get_builder()
+                builder.run()
