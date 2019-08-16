3196359c31b6ef648eb7816b8b3b9764f264eea5
Uilian Ries <uilianries@gmail.com>
2019-08-16 17:16:55 -0300

    Support stable branch
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/entry.py
+++ .ci/entry.py
@@ -3,12 +3,23 @@
 
 import json
 import subprocess
+import os
+
+
+def get_channel_name():
+    return os.getenv("CONAN_CHANNEL", "testing")
+
+
+def get_username():
+    return os.getenv("CONAN_USERNAME", "bincrafters")
 
 
 if __name__ == "__main__":
+    channel = get_channel_name()
+    namespace = "{}/{}".format(get_username(), get_channel_name())
     with open("groups.json") as json_file:
         json_data = json.load(json_file)
         for index in range(6):
             for package in json_data[str(index)]:
                 recipe = "conanfile-{}.py".format(package.lower())
-                subprocess.check_call(["conan", "export", recipe, "bincrafters/testing"])
+                subprocess.check_call(["conan", "export", recipe, namespace])
